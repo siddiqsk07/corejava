47424daf48ed095117e6481e004dd61bb3a3dda2
siddiqsk07 <siddiqshaik2772@gmail.com>
2025-11-19 20:11:12 +0530

    Create Studentresult.java

---
+++ src/net/konic/corejava/variables/Studentresult.java
@@ -0,0 +1,25 @@
+package net.konic.corejava.variables;
+
+public class Studentresult {
+	public static void main(String[] args) {
+		String studentName="muhammed";
+		int age=15;
+		int standard=10;
+		float percentage=78.5f;
+		String institution="kkr";
+		
+		System.out.println(studentName);
+		System.out.println(age);
+		System.out.println(percentage);
+		System.out.println(standard);
+		System.out.println(institution);
+		
+		int teluguMarks=99;
+		int hindiMarks=95;
+		int englishMarks=90;
+		int mathsMarks=75;
+		int physicsMarks=80;
+		int socialMarks=88;
+		
+	}
+}
