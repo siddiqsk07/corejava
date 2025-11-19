cbf4eb0c437ea03b545a3eb2f09449fb1c26941d
siddiqsk07 <siddiqshaik2772@gmail.com>
2025-11-19 11:05:45 +0530

    commit intialize

--- src/net/konic/corejava/variables/FirstProgram.java
+++ src/net/konic/corejava/variables/FirstProgram.java
@@ -119,5 +119,6 @@ public static void main(String args[]) {
 	  
 	  
 	  
+	  
 }
 }
---
+++ src/net/konic/corejava/variables/Wifi.java
@@ -0,0 +1,22 @@
+package net.konic.corejava.variables;
+
+public class Wifi {
+	
+	public static void main(String[] args) {
+		String networkName="Airtel";
+		String brandName="Airtel xtream";
+		int networkSpeed=100;
+		String connectivity="Connected";
+		int range=45;
+		
+		System.out.println(networkName);
+		System.out.println(brandName);
+		System.out.println(networkSpeed);
+		System.out.println(connectivity);
+		System.out.println(range);
+		
+		
+		
+	}
+
+}
