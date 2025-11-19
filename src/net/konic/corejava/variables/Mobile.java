88f9ffd1c3f8e498c50367de7dd753c6a7865936
siddiqsk07 <siddiqshaik2772@gmail.com>
2025-11-19 11:29:00 +0530

    commit intialize

---
+++ src/net/konic/corejava/variables/Mobile.java
@@ -0,0 +1,23 @@
+package net.konic.corejava.variables;
+
+public class Mobile {
+	public static void main(String[] args) {
+		String brandName="One Plus 12r";
+		int ram=16;
+		int storage=256;
+		int cameraPixel=50;
+		String operatingSystem="OxygenOS15";
+		float displaySize=17.22f;
+		int androidVersion=15;
+		
+		System.out.println(brandName);
+		System.out.println(ram);
+		System.out.println(storage);
+		System.out.println(operatingSystem);
+		System.out.println(cameraPixel);
+		System.out.println(displaySize);
+		System.out.println(androidVersion);
+		
+	}
+
+}
