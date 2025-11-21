96ae756539d859ed19505239a6b30abe511a0be3
siddiqsk07 <siddiqshaik2772@gmail.com>
2025-11-21 22:30:40 +0530

    statement commit

---
+++ src/net/konic/corejava/operators/BankTokens.java
@@ -0,0 +1,22 @@
+package net.konic.corejava.operators;
+
+import java.util.Scanner;
+
+public class BankTokens {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int token=100;
+		
+		System.out.println("Tokens Issued :");
+		int couont=sc.nextInt();
+		
+		 if (count >= 1) {
+	            token++;
+	            System.out.println("Token: " + token);
+	        }
+			
+		}
+		}
+
+
