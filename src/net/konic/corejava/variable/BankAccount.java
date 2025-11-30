b5805079eba74d01655e5ff5f8606d0d420d0a7f
siddiqsk07 <siddiqshaik2772@gmail.com>
2025-11-30 23:27:10 +0530

    commit

---
+++ src/net/konic/corejava/variable/BankAccount.java
@@ -0,0 +1,23 @@
+package net.konic.corejava.variable;
+
+
+
+public class BankAccount {
+	int balance;
+	
+	public void deposite(int amount) {
+		balance = balance + amount;
+		
+	}
+	
+	public int getbalance() {
+		return balance;
+	}
+	public static void main(String[] args) {
+		BankAccount acc=new BankAccount();
+		acc.deposite(1200);
+		int bal=acc.getbalance();
+		System.out.println("Balance :"+bal);
+	}
+	
+}
